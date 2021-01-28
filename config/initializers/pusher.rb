18ebf91b5b61b693e382f61c7f0ccf69785bb980
Aimery Cayol <aimerycayol@gmail.com>
2021-01-29 00:14:16 +0100

    au dodo

--- config/initializers/pusher.rb
+++
@@ -1,8 +0,0 @@
-require 'pusher'
-
-Pusher.app_id = '1145768'
-Pusher.key = '44102e972411b76f0a55'
-Pusher.secret = 'ae9e2da193403daf8346'
-Pusher.cluster = 'eu'
-Pusher.logger = Rails.logger
-Pusher.encrypted = true
